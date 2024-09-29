3e66e409a09a6a30ae9eae36279318da5c4121dd
KostiantynKrasnostup <vladovskiy462@gmail.com>
2024-09-29 16:13:32 +0300

    Add files via upload

---
+++ Індивідуальне авдання. Варіант - 18/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Структура Country, що містить поля для назви країни, кількості населення та частини світу
+    struct Country
+    {
+        public string name;
+        public int population;
+        public string partOfTheWorld;
+
+        // Метод, що повертає рядок із значеннями полів
+        public string Vivod()
+        {
+            return $"{name} {population} {partOfTheWorld}";
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Створити список країн
+        List<Country> countries = new List<Country>();
+        Country country;
+        int n = 4;
+
+        // Ввести дані з клавіатури
+        Console.WriteLine("Введіть дані:");
+        for (int i = 0; i < n; i++)
+        {
+            country.name = Console.ReadLine();
+            country.population = int.Parse(Console.ReadLine());
+            country.partOfTheWorld = Console.ReadLine();
+            countries.Add(country);
+        }
+
+        // Вивести список країн на екран
+        Console.WriteLine("Список країн:");
+        foreach (var c in countries)
+        {
+            Console.WriteLine(c.Vivod());
+        }
+
+        // Ввести частину світу з клавіатури
+        Console.Write("Введіть частину світу: ");
+        string part = Console.ReadLine();
+
+        // Знайти кількість країн у цій частині світу та загальну кількість населення
+        int count = 0;
+        int totalPopulation = 0;
+        foreach (var c in countries)
+        {
+            if (c.partOfTheWorld == part)
+            {
+                count++;
+                totalPopulation += c.population;
+            }
+        }
+
+        // Вивести результати
+        Console.WriteLine($"\nКількість країн у цій частині світу: {count}");
+        Console.WriteLine($"Загальна кількість населення країнах цієї частини світу: {totalPopulation}");
+
+        Console.ReadKey();
+    }
+}
---
+++ Приклад 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    struct Lud
+    {
+        public string f;
+        public int gr;  
+        public char p;
+
+        public string Vivod()
+        {
+            return $"{f} {gr} {p}";
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        List<Lud> students = new List<Lud>();
+        Lud stud;
+        Lud mx = new Lud();
+        int n = 4;
+
+        Console.WriteLine("Введіть дані:");
+        for (int i = 0; i < n; i++)
+        {
+            stud.f = Console.ReadLine();
+            stud.gr = int.Parse(Console.ReadLine());  
+            stud.p = char.Parse(Console.ReadLine());  
+            students.Add(stud);
+        }
+
+        Console.WriteLine("Список студентів:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(students[i].Vivod());
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (students[i].p == 'ж' &&
+                (mx.gr == 0 || students[i].gr < mx.gr))  
+            {
+                mx = students[i];
+            }
+        }
+
+        if (mx.gr != 0)
+            Console.WriteLine("\nНаймолодша студентка: {0} {1} {2}",
+                mx.f, mx.gr, mx.p);
+        else
+            Console.WriteLine("\nНемає дівчин");
+
+        Console.ReadKey();
+    }
+}
